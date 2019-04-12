779beba0ae620de537b2e4b912c1d4ad6c2390d4
Md. Alamin Mahamud <alamin.ineedahelp@gmail.com>
2019-04-12 18:50:25 +0600

    version

---
+++ salesforce_client/__version__.py
@@ -0,0 +1,14 @@
+"""Version Details for salesforce_client
+
+This file shamelessly taken from the requests library"""
+
+__title__ = 'salesforce_client'
+__description__ = 'A Basic Salesforce REST API Client'
+__url__ = 'https://github.com/pylab-bd/salesforce-api-client'
+__version__ = '0.1.0'
+__author__ = 'Alamin Mahamud'
+__author_email__ = 'alamin.ineedahelp@gmail.com'
+__license__ = 'Apache 2.0'
+__maintainer__ = 'Demian Brecht'
+__maintainer_email__ = 'demianbrecht@gmail.com'
+__keywords__ = 'python salesforce salesforce.com'
